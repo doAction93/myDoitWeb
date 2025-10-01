3946d812568b38decd027bb5023d3e65728bb5b1
doAction93 <doaction93@gmail.com>
2025-10-02 00:46:48 +0900

    최종 업로드

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,23 @@
-
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    async headers() {
+      return [
+        {
+          source: "/img/:path*",
+          headers: [
+            { key: "Cache-Control", value: "public, max-age=31536000, immutable" }
+          ]
+        },
+        {
+          source: "/thumbNail/:path*",
+          headers: [
+            { key: "Cache-Control", value: "public, max-age=31536000, immutable" }
+          ]
+        }
+      ];
+    }
+  };
+  
+  export default nextConfig;
+  
