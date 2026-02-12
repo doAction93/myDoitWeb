813084a267f86324c92f99c6ac2b041562f714a5
doAction93 <doaction93@gmail.com>
2026-02-12 18:02:44 +0900

    add shoppingList.json for CDN

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,15 @@ const nextConfig = {
     async headers() {
       return [
         {
+          source: "/shoppingList.json",
+          headers: [
+            {
+              key: "Cache-Control",
+              value: "public, max-age=600, s-maxage=86400, stale-while-revalidate=604800"
+            }
+          ]
+        },
+        {
           source: "/img/:path*",
           headers: [
             { key: "Cache-Control", value: "public, max-age=31536000, immutable" }
